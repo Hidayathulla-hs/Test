6b5b7193bf3ea7ac04c398d5578b4969941c37b4
SHAIK HIDAYATHULLA <shaikhidayath265@gmail.com>
2026-02-09 07:19:42 +0000

    commit

--- models/staging/pre_post_model.sql
+++ models/staging/pre_post_model.sql
@@ -3,6 +3,21 @@
     incremental_strategy = "append",
     pre_hook = "TRUNCATE TABLE {{ this }}"
 ) }}
+WITH source_data AS (
+
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY id
+               ORDER BY order_date DESC
+           ) AS rn
+    FROM {{ source('source_table_name','orders') }}
+
+)
 
 SELECT *
-FROM {{ source('source_table_name','orders') }}
+FROM source_data
+WHERE rn = 1
+
+{% if is_incremental() %}
+AND order_date > (SELECT MAX(order_date) FROM {{ this }})
+{% endif %}
