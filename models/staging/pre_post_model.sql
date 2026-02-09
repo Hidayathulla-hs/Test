b18d4327d45e5c9a29d87b9e6798ea86c08ac99a
SHAIK HIDAYATHULLA <shaikhidayath265@gmail.com>
2026-02-09 06:44:59 +0000

    commit

--- models/staging/pre_post_model.sql
+++ models/staging/pre_post_model.sql
@@ -1,9 +1,8 @@
 {{ config(
     materialized = "incremental",
-    incremental_strategy = "append"
-     pre_hook="TRUNCATE TABLE {{ this }}",
+    incremental_strategy = "append",
+    pre_hook = "TRUNCATE TABLE {{ this }}"
 ) }}
 
 SELECT *
-FROM {{ source('source_table_name', 'orders') }}
-
+FROM {{ source('source_table_name','orders') }}
